34138c1c9edc75ce667efa247ec0d3b81a0d027a
miily8310s <rillakerika@gmail.com>
2021-07-07 07:44:42 +0900

    Add div function

--- hello.go
+++ hello.go
@@ -4,6 +4,15 @@ import (
 	"fmt"
 )
 
+func div(a, b int) (int, int) {
+  q := a / b
+  r := a % b
+  return q, r
+}
+
+
 func main()  {
 	fmt.Println("Hello World!")
+  a, b := div(19, 7) 
+	fmt.Println(a, b) // 2, 5
 }
