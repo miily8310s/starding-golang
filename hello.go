be91ce44ae3b1f1850b8fa73791d1929270f3b57
miily8310s <rillakerika@gmail.com>
2021-07-09 08:47:30 +0900

    Add if and switch

--- hello.go
+++ hello.go
@@ -17,6 +17,17 @@ func main()  {
   c := func (x, y int) int {
     return x + y + 2
   }
+  d := 10
+  n := 3
+  if d % 2 == 0 {
+    fmt.Println("even number!")
+  }
+  switch n {
+    case 1, 2:
+      fmt.Println("1 or 2")
+    case 3, 4:
+      fmt.Println("3 or 4")
+  }
   fmt.Println(a, b) // 2, 5
   fmt.Println(c(19, 2))
 }
