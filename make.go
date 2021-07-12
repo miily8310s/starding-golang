612b9720d0c1675aadf9a532f0a775287e88e633
miily8310s <rillakerika@gmail.com>
2021-07-13 08:09:16 +0900

    Add map

--- make.go
+++ make.go
@@ -9,7 +9,15 @@ func main()  {
 	a := s[0:4]
 	fir := [] int {1, 2, 3}
 	fir = append(fir, 4)
+	m := make(map[int]string)
+	m[1] = "US"
+	m[81] = "China"
+	m[47] = "Japan"
 	fmt.Println(s)
 	fmt.Println(a)
 	fmt.Println(fir)
+	fmt.Println(m)
+	for k, v := range m {
+		fmt.Printf("%d => %s\n", k, v)
+	}
 }
